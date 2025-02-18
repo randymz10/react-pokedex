3e850058d38469e195ac05544c651b15439096fd
randymz10 <randymz10@gmail.com>
2025-02-18 01:00:04 -0300

    Fix error into the pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -17,21 +17,24 @@ function Pagination() {
     setIsDropdownActive((prev) => !prev);
   }
 
-  function handlePrev(page) {
+  function handlePrev() {
+    const page = currentPage - 1;
     updatePokemons(prevUrl, page);
   }
 
-  function handleNext(page) {
+  function handleNext() {
+    const page = currentPage + 1;
     updatePokemons(nextUrl, page);
   }
 
   function handlePage(page) {
+    console.log(page);
     const url = `${apiUrl}?offset=${(page - 1) * 20}`;
     updatePokemons(url, page);
   }
 
   return (
-    <div className="columns is-centered is-mobile">
+    <div className="columns is-centered is-mobile mt-1">
       <div className="column is-half-mobile is-one-third-tablet is-one-quarter-desktop">
         <nav
           className="pagination is-centered"
@@ -65,35 +68,36 @@ function Pagination() {
                   aria-controls="dropdown-menu"
                   onClick={handleDropdown}
                 >
-                  <span>Page: {currentPage} of / {numOfPages}</span>
+                  <span>Select Page</span>
                   <span className="icon is-small">
                     <FaAngleUp />
                   </span>
                 </button>
               </div>
-              <div>
-                <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                  <div className="dropdown-content">
-                    {Array.from({ length: numOfPages }, (_, index) => {
-                      const page = index + 1;
-                      return (
-                        <a
-                          key={page}
-                          onClick={() => handlePage(page)}
-                          className={`dropdown-item ${
-                            currentPage === page ? "is-active" : ""
-                          }`}
-                        >
-                          {page}
-                        </a>
-                      );
-                    })}
-                  </div>
+              <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content">
+                  {Array.from({ length: numOfPages }, (_, index) => {
+                    const page = index + 1;
+                    return (
+                      <a
+                        key={page}
+                        onClick={() => handlePage(page)}
+                        className={`dropdown-item ${
+                          currentPage === page ? "is-active" : ""
+                        }`}
+                      >
+                        {page}
+                      </a>
+                    );
+                  })}
                 </div>
               </div>
             </div>
           </div>
         </nav>
+        <div className="content has-text-centered">
+          {`Page ${currentPage} of ${numOfPages}`}
+        </div>
       </div>
     </div>
   );
--- src/store/pokemonStore.js
+++ src/store/pokemonStore.js
@@ -23,25 +23,43 @@ export const usePokemonStore = create((set) => ({
     fetch(url)
       .then((res) => res.json())
       .then(async (data) => {
-        const pokemonInfoList = data.results.map(async (pokemonData) => {
-          const res = await fetch(pokemonData.url);
-          const data = await res.json();
-          const pokemonInfo = {
-            id: data.id,
-            name: data.name,
-            imageUrl: data.sprites.other["official-artwork"].front_default,
-            types: data.types,
-          };
-          return pokemonInfo;
-        });
+        let pokemonInfoList;
+        if (data.results) {
+          pokemonInfoList = data.results.map(async (pokemonData) => {
+            const res = await fetch(pokemonData.url);
+            const data = await res.json();
+            const pokemonInfo = {
+              id: data.id,
+              name: data.name,
+              imageUrl: data.sprites.other["official-artwork"].front_default,
+              types: data.types,
+            };
+            return pokemonInfo;
+          });
+        }
+
+        if (data.pokemon) {
+          pokemonInfoList = data.pokemon.map(async (pokemonData) => {
+            const res = await fetch(pokemonData.url);
+            const data = await res.json();
+            const pokemonInfo = {
+              id: data.id,
+              name: data.name,
+              imageUrl: data.sprites.other["official-artwork"].front_default,
+              types: data.types,
+            };
+            return pokemonInfo;
+          });
+        }
+
         set((prevState) => ({
           paginationData: {
             ...prevState.paginationData,
             currentUrl: url,
-            prevUrl: data.previous,
-            nextUrl: data.next,
-            numOfPokemons: data.count,
-            currentPage: page,
+            prevUrl: data.previous ? data.previous : null,
+            nextUrl: data.next ? data.next : null,
+            numOfPokemons: data.count ? data.count : data.pokemon.length,
+            currentPage: page ? page : 1,
           },
         }));
         set({ pokemons: await Promise.all(pokemonInfoList) });
